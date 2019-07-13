747108c5b744ccfc0c4594000eb6c74ae7b0a680
dinukadev <dinuka.arseculeratne@gmail.com>
2019-07-13 18:29:15 +1000

    nlp example

---
+++ nlp/word_tokenize.py
@@ -0,0 +1,7 @@
+from nltk.tokenize import word_tokenize
+
+sent = '''I am reading a book.
+          It is Python Machine Learning By Example,
+          2nd Edition.'''
+
+print(word_tokenize(sent))
