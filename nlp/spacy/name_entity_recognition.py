ca966a0ec661aad63f857a6f5a5b3e15b2fe4f27
dinukadev <dinuka.arseculeratne@gmail.com>
2019-07-13 19:00:59 +1000

    spacy ner details

--- nlp/spacy/name_entity_recognition.py
+++ nlp/spacy/name_entity_recognition.py
@@ -4,3 +4,6 @@ nlp = spacy.load('en_core_web_sm')
 sent = 'The book written by Hayden Liu in 2018 sold at $30 in America'
 tokens = nlp(sent)
 print([(token.text, token.label_) for token in tokens.ents])
+
+# Please refer to https:/ / spacy. io/ api/ annotation#sectionnamed-
+# entities for a full list of named entity tags.
